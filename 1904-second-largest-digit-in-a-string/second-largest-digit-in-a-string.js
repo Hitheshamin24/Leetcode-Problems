0b2ece50f09949ef7abc93ec54d35a4597ca4268
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2025-09-30 11:16:13 +0530

    Time: 0 ms (100.00%) | Memory: 55.5 MB (68.49%) - LeetSync

--- 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.js
+++ 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.js
@@ -3,18 +3,21 @@
  * @return {number}
  */
 var secondHighest = function(s) {
-    let digits=s.match(/[0-9]/g)?.map(Number) || [];
     
-    let largest=digits[0];
+    
+    let largest=-1;
     let secondLargest=-1;
-    for(let i=0;i<digits.length;i++){
-        if(largest<digits[i])
+    for(let i=0;i<s.length;i++){
+        let code = s.charCodeAt(i)-48
+        if(code >=0 && code <= 9 ){ 
+        if(largest<code)
         {
             secondLargest=largest
-            largest=digits[i]
+            largest=code
         }
-        else if(largest > digits[i] && secondLargest < digits[i] ){
-            secondLargest=digits[i];
+        else if(largest > code && secondLargest < code ){
+            secondLargest=code;
+        }
         }
     }
     return secondLargest;
