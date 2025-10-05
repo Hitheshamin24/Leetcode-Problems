c5030de77d12e1f0655c2c22605dc21921e424a4
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2025-10-05 15:46:18 +0530

    Time: 3 ms (61.63%) | Memory: 65.4 MB (50.65%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -8,7 +8,7 @@ var maxProfit = function (prices) {
     for (let i = 1; i < prices.length; i++) {
 
         maxProfit = Math.max(maxProfit, (prices[i] - bestBuy))
-        if (bestBuy > prices[i]) {
+        if (prices[i] < bestBuy) {
             bestBuy = prices[i]
         }
     }
