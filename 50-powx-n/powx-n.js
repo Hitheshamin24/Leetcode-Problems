0de6247802d659d9687e036d103bf7beb3d3f00c
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2025-10-05 15:15:23 +0530

    Time: 0 ms (100.00%) | Memory: 54.9 MB (28.83%) - LeetSync

--- 50-powx-n/powx-n.js
+++ 50-powx-n/powx-n.js
@@ -8,8 +8,8 @@ var myPow = function (x, n) {
     if (n == 0) return 1.0;
     if (x == 0) return 0;
     if (n == 1) return x;
-    if (x == 1 && n % 2 != 0 && n<0) return -1.0;
-    if (x == 1 && n % 2 == 0 && n<0) return 1.0;
+    if (x == -1 && n % 2 != 0 ) return -1.0;
+    if (x == -1 && n % 2 == 0 ) return 1.0;
     let binaryform = n;
 
     if (n < 0) {
