64de1c0c9bd0932bb5e197e7a781f46fb6a32012
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2025-10-07 20:37:10 +0530

    Time: 2 ms (69.43%) | Memory: 58.5 MB (94.88%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.js
+++ 485-max-consecutive-ones/max-consecutive-ones.js
@@ -2,8 +2,8 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMaxConsecutiveOnes = function (nums) {
-  let currentCount=0,maxCount=0
+var findMaxConsecutiveOnes = function(nums) {
+    let currentCount=0,maxCount=0
     for(let i=0;i<nums.length;i++){
         if(nums[i]!==0){
             currentCount++
