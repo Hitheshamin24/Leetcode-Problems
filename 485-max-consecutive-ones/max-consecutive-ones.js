76331582768c3dcfb8185831753f29d4603a1ae8
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2025-10-07 20:31:35 +0530

    Time: 3 ms (33.60%) | Memory: 60.9 MB (14.08%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.js
+++ 485-max-consecutive-ones/max-consecutive-ones.js
@@ -2,16 +2,16 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMaxConsecutiveOnes = function(nums) {
-    let temp=0
-    let maxCount=0
-    for(i=0;i<nums.length;i++){
-        if(nums[i]!=0){
+var findMaxConsecutiveOnes = function (nums) {
+    let temp = 0
+    let maxCount = 0
+    for (i = 0; i < nums.length; i++) {
+        if (nums[i] != 0) {
             temp++;
-        }else{
-            temp=0
+        } else {
+            maxCount = Math.max(temp, maxCount)
+            temp = 0
         }
-        maxCount=Math.max(temp,maxCount)
     }
-    return maxCount;
+    return Math.max(maxCount, temp);
 };
