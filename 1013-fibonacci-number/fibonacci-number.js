98500c77e0bbac401f189d1d0f0988983e6d7430
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2025-10-11 10:44:43 +0530

    Time: 50 ms (59.65%) | Memory: 54.2 MB (28.42%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -3,9 +3,7 @@
  * @return {number}
  */
 var fib = function (n) {
-    // const sqrt5=Math.sqrt(5);
-    // const fibN=Math.pow((1+sqrt5)/2,n)-Math.pow((1-sqrt5)/2,n)
-    // return Math.round(fibN/sqrt5);
+   
     const array=[0,1];
     if(n<2){
        return n;
