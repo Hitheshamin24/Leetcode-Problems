75870bfdeeaaf09417053d86fc302cb0f85bb096
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2025-10-14 12:56:03 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (35.75%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.js
+++ 367-valid-perfect-square/valid-perfect-square.js
@@ -3,13 +3,19 @@
  * @return {boolean}
  */
 var isPerfectSquare = function (num) {
-    let bool = false
-    if (num == 1) return true
-    for (let i = 1; i <= num / 2; i++) {
-        if (i * i == num) {
-            bool = true
-            return bool
+    let left = 1 ,right = num
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2)
+        let square = mid * mid
+        if (square == num) {
+            return true
+        }
+        else if (square < num) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1
         }
     }
-    return bool
+    return false
 };
