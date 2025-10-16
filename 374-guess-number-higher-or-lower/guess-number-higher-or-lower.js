adedec225c4a696b49043d25dbdb90a8e4b78673
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2025-10-16 18:05:43 +0530

    Time: 43 ms (58.34%) | Memory: 53.8 MB (49.88%) - LeetSync

--- 374-guess-number-higher-or-lower/guess-number-higher-or-lower.js
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.js
@@ -15,8 +15,9 @@ var guessNumber = function (n) {
     let left = 0, right = n
     while (left <= right) {
         let mid = Math.floor((left + right) / 2)
-        if (guess(mid) == 0) return mid;
-        else if (guess(mid) == -1) {
+        let result=guess(mid)
+        if (result == 0) return mid;
+        else if (result == -1) {
             right = mid - 1
         } else {
             left = mid + 1
