292f99494891764e120e43debd7ca98adf7c78cb
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2025-10-16 18:22:56 +0530

    Time: 58 ms (6.39%) | Memory: 54.7 MB (9.56%) - LeetSync

--- 278-first-bad-version/first-bad-version.js
+++ 278-first-bad-version/first-bad-version.js
@@ -18,18 +18,14 @@ var solution = function (isBadVersion) {
      * @return {integer} The first bad version
      */
     return function (n) {
-        let l = 0;
-        let r = n;
-        while (l < r) {
-            let m = l + Math.floor((r - l) / 2)
-            let version = isBadVersion(m)
-            if (version) {
-                r = m
-
-            } else {
-                l = m + 1
+        let left = 1, right = n;
+        while (left < right) {
+            mid = Math.floor((left + right) / 2)
+            if (isBadVersion(mid)) right= mid
+            else {
+                left = mid + 1
             }
         }
-        return l;
+        return left
     };
 };
