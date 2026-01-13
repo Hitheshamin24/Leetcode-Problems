c97b50566858fc954b02815860b8133afeb23901
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2026-01-13 20:02:51 +0530

    Time: 0 ms (100.00%) | Memory: 58 MB (46.51%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -5,19 +5,16 @@
  */
 var search = function (nums, target) {
     let left = 0, right = nums.length - 1;
-    let flag = false
     while (left <= right) {
-        let mid = Math.floor((left + right) / 2)
-        if (nums[mid] == target) {
-            flag = true
+        const mid = left + Math.floor((right - left) / 2)
+        if(nums[mid]==target){
             return mid;
-        } else if (nums[mid] < target) {
-            left = mid + 1
-        } else {
-            right = mid - 1
+        }
+        else if (nums[mid]>target){
+            right=mid-1
+        }else{
+            left=mid+1
         }
     }
-    if (!flag) {
-        return -1
-    }
+    return -1
 };
