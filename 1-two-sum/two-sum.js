d59f831c72156d51e2f5ab4c71469ccc569495e5
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2026-01-19 20:35:22 +0530

    Time: 2 ms (68.65%) | Memory: 54.8 MB (66.88%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    const hash = new Map();
+    for(let i=0;i<nums.length;i++){
+        const need=target-nums[i];
+        if(hash.has(need)){
+            return [hash.get(need),i]
+        }
+        hash.set(nums[i],i)
+    }
+
+};
