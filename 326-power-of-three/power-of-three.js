12f9219797c4a9b88a6f7e70c28f1c07044c0ed5
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2026-02-02 20:57:38 +0530

    Time: 11 ms (21.76%) | Memory: 65.5 MB (42.16%) - LeetSync

---
+++ 326-power-of-three/power-of-three.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    
+    for(let i=0;3**i<=n;i++){
+        if(3**i==n){
+            return true;
+        }
+    }
+    return false;
+};
