6d4f4ae24688c83edf320d4b84c365e34fd86a2b
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2026-02-02 21:02:25 +0530

    Time: 2 ms (6.91%) | Memory: 55.6 MB (80.61%) - LeetSync

---
+++ 342-power-of-four/power-of-four.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    for(let i=0;4**i<=n;i++){
+        if(4**i==n){
+            return true;
+        }
+    }
+    return false
+};
