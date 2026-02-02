552503097c5c1bccfa98def6cbdb28d2ffa85c13
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2026-02-02 20:55:53 +0530

    Time: 1 ms (31.45%) | Memory: 57.7 MB (6.20%) - LeetSync

--- 231-power-of-two/power-of-two.js
+++ 231-power-of-two/power-of-two.js
@@ -3,9 +3,8 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    if((n<-(2**31)) ||(n>2**31 - 1)){
-        return -1;
-    }
+    if(n<0 ) return false;
+    if(n==1) return true;
     for(let i=0;2**i<=n;i++){
         if(2**i==n){
             return true
