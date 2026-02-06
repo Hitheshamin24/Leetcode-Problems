bc1316bc5994c1d84d05d333404d04b343bd4fa7
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2026-02-06 20:21:02 +0530

    Time: 0 ms (100.00%) | Memory: 61.9 MB (32.77%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -2,6 +2,12 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-var reverseString = function(s) {
-    return s.reverse();
+var reverseString = function (s) {
+    let half = Math.floor(s.length / 2)
+    let j = s.length - 1
+    for (let i=0;i<half;i++ ){
+        let temp=s[i]
+        s[i]=s[j-i]
+        s[j-i]=temp
+    }
 };
