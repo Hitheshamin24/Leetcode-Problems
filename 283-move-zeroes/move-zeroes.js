7e61bf04607ddd52e8c8b7d1a108abc53e991db4
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2026-02-09 21:00:33 +0530

    Time: 2 ms (50.99%) | Memory: 60.5 MB (50.38%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -2,15 +2,16 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function (nums) {
-    let k = 0;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] != 0) {
-            nums[k] = nums[i];
-            k++;
+var moveZeroes = function(nums) {
+    let j=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]!=0){
+            nums[j++]=nums[i]
+
         }
+
     }
-    for (i = k; i < nums.length; i++) {
-        nums[i] = 0;
+    for(let i=j;i<nums.length;i++){
+        nums[j++]=0
     }
 };
