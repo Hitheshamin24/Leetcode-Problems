630c1da1ef6274798a213a1e7e502437b14c7711
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2026-02-21 21:28:16 +0530

    Time: 293 ms (5.55%) | Memory: 61.6 MB (11.11%) - LeetSync

---
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+var countPrimeSetBits = function (left, right) {
+    const primes=new Set([2,3,5,7,11,13,17,19])
+    let count=0
+    for(let i=left;i<=right;i++){
+        const bits=i.toString(2).split('0').join('').length
+
+        if(primes.has(bits)){
+            count++
+        }
+    }
+    return count
+};
