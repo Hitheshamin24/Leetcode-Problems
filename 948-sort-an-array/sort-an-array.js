4c825d3326e75424af40104d7ff83df9309cd790
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2026-02-27 21:35:21 +0530

    Time: 124 ms (46.84%) | Memory: 81.4 MB (42.60%) - LeetSync

--- 948-sort-an-array/sort-an-array.js
+++ 948-sort-an-array/sort-an-array.js
@@ -1,39 +1,23 @@
 /**
  * @param {number[]} nums
  * @return {number[]}
- */var sortArray = function (nums) {
-    const quickSort = (nums, start, end) => {
-        if (start >= end) return;
-        
-        let pivotIndex = partition(nums, start, end);
-        quickSort(nums, start, pivotIndex - 1);
-        quickSort(nums, pivotIndex + 1, end);
-    };
-    
-    const partition = (nums, start, end) => {
-        // --- START OF FIX ---
-        // 1. Pick a random index from the current sub-array.
-        const randomIndex = start + Math.floor(Math.random() * (end - start + 1));
-        
-        // 2. Swap the random element with the last element.
-        [nums[randomIndex], nums[end]] = [nums[end], nums[randomIndex]];
-        // --- END OF FIX ---
-
-        // Now, the rest of the function works perfectly with the new, random pivot.
-        let pivot = nums[end];
-        let i = start - 1;
-
-        for (let j = start; j < end; j++) {
-            if (pivot > nums[j]) {
-                i++;
-                [nums[i], nums[j]] = [nums[j], nums[i]];
-            }
-        }
-        i++;
-        [nums[end], nums[i]] = [nums[i], nums[end]];
-        return i;
-    };
-
-    quickSort(nums, 0, nums.length - 1);
-    return nums;
+ */
+var sortArray = function(nums) {
+    if (nums.length <= 1) return nums;
+    let mid = Math.floor(nums.length / 2);
+    let left = sortArray(nums.slice(0, mid));
+    let right = sortArray(nums.slice(mid));
+    return merge(left, right);
 };
+
+function merge(left, right) {
+    let res = [], i = 0, j = 0;
+    while (i < left.length && j < right.length) {
+        if (left[i] < right[j]) {
+            res.push(left[i++]);
+        } else {
+            res.push(right[j++]);
+        }
+    }
+    return [...res, ...left.slice(i), ...right.slice(j)];
+}   
