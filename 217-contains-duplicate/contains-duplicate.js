7a9e6941e58a1c1b5df5fb0bbee3103c4061fd92
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2026-06-02 09:27:33 +0530

    Time: 19 ms (47.76%) | Memory: 73.1 MB (86.60%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -2,20 +2,8 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-    for(var i = 0; i < nums.length-1; i++){
-        for(var j = i+1; j > 0; j--){
-            if(nums[j] == nums[j-1]){
-                return true;
-            }else if(nums[j] < nums[j-1]){
-                var temp = nums[j];
-                nums[j] = nums[j-1];
-                nums[j-1] = temp;
-            }else{
-                break;
-            }
-        }
-    }
-
-    return false;
+var containsDuplicate = function (nums) {
+    let set = new Set(nums)
+    if (set.size === nums.length) return false
+    else return true;
 };
