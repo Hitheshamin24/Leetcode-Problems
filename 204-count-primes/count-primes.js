a734232c685d92d8da32d7f84b53eba7f3330ba3
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2026-06-06 09:46:24 +0530

    Time: 178 ms (53.58%) | Memory: 94.8 MB (79.84%) - LeetSync

---
+++ 204-count-primes/count-primes.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var primeArray = function (n) {
+    let arr = new Array(n + 1).fill(true);
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (arr[i]) {
+            for (let j = i * i; j <= n; j += i) {
+                arr[j] = false
+            }
+        }
+    }
+    return arr
+}
+var countPrimes = function (n) {
+    
+    let arr=primeArray(n)
+    let count=0
+    for(let i=2;i<arr.length-1;i++){
+        if(arr[i]) count++
+    }
+    return count
+
+};
