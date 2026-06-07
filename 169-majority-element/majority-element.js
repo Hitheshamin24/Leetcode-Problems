7a7848fc95df15a2adb52ccdeae600bd0b799c2a
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2026-06-07 09:34:16 +0530

    Time: 1 ms (88.23%) | Memory: 56.1 MB (76.68%) - LeetSync

---
+++ 169-majority-element/majority-element.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let frequency=0,ans=0;
+    for(let i=0;i<nums.length;i++){
+        if(frequency==0) ans=nums[i]
+        if(ans==nums[i]) frequency++
+        else{
+            frequency--
+        }
+    }
+    return ans
+};
