998205800e353a2486099e96c74750a81e05569b
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2026-06-08 10:11:46 +0530

    Time: 27 ms (77.62%) | Memory: 88.8 MB (37.76%) - LeetSync

---
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} pivot
+ * @return {number[]}
+ */
+var pivotArray = function (nums, pivot) {
+    let arr = []
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < pivot) arr.push(nums[i])
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == pivot) arr.push(nums[i])
+    } 
+    // arr.push(pivot)
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > pivot) arr.push(nums[i])
+    }
+    return arr
+};
