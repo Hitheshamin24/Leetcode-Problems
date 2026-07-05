0df8b74778e6f6a2d5dd32ca927a6395902fd7ea
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2026-07-05 09:12:10 +0530

    Time: 1 ms (65.53%) | Memory: 55.5 MB (57.97%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function (height) {
+    let left = 0, right = height.length - 1, leftMax = rightMax = total = 0
+    while (left < right) {
+        leftMax = Math.max(height[left], leftMax)
+        rightMax = Math.max(height[right], rightMax)
+
+        if (leftMax < rightMax) total += Math.min(leftMax, rightMax) - height[left++]
+        else total += Math.min(leftMax, rightMax) - height[right--]
+
+    }
+    return total
+};
