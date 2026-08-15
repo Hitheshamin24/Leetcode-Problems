32254caff42f4176ea7ef9bb2a9b10cc7bbf944b
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2026-08-15 09:46:56 +0530

    Time: 4 ms (44.91%) | Memory: 55 MB (94.03%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.js
+++ 1168-duplicate-zeros/duplicate-zeros.js
@@ -2,27 +2,26 @@
  * @param {number[]} arr
  * @return {void} Do not return anything, modify arr in-place instead.
  */
+
 var countZero = (arr) => {
-    let count = 0;
+    let count = 0
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === 0) count++
     }
     return count
 }
+
 var duplicateZeros = function (arr) {
-    let i = arr.length - 1
-    let j = arr.length - 1 + countZero(arr)
+    let i = arr.length - 1;
+    let j = i + countZero(arr)
     while (i >= 0) {
-        if (j < arr.length) {
-            arr[j] = arr[i]
-        }
+        if (j < arr.length) arr[j] = arr[i]
         j--
-        if (arr[i] === 0) {
+        if (arr[i] == 0) {
             if (j < arr.length) arr[j] = 0
             j--
         }
         i--
 
     }
-    
-};
+}
