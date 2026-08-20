18b1b6ea9b1629da717bff5de31d00157ef5a0a1
HITHESH <168573315+Hitheshamin24@users.noreply.github.com>
2026-08-20 10:17:09 +0530

    Time: 1 ms (71.88%) | Memory: 56 MB (96.88%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.js
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.js
@@ -5,12 +5,10 @@
 var resultArray = function (nums) {
     let arr1 = [nums[0]]
     let arr2 = [nums[1]]
-    let k = 1, j = 1;
     for (let i = 2; i < nums.length; i++) {
-        if (arr1[j-1] > arr2[k-1]) {
-            arr1[j++] = nums[i]
-        } else arr2[k++] = nums[i]
-
+        if (arr1[arr1.length - 1] > arr2[arr2.length - 1])
+        arr1.push(nums[i])
+        else arr2.push(nums[i])
     }
-    return [...arr1,...arr2]
+return arr1.concat(arr2)
 };
